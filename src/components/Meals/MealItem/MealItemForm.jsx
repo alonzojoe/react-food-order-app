98b3648cdd4097696f4fe525454e3f0a209717a5
Joenell Alonzo <72643848+alonzojoe@users.noreply.github.com>
2024-02-13 16:57:09 +0800

    Added a meal item form to the meal item component.

--- src/components/Meals/MealItem/MealItemForm.jsx
+++ src/components/Meals/MealItem/MealItemForm.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import Input from "../../UI/Input";
+import classes from "./MealItemForm.module.css";
 
 const MealItemForm = () => {
-  return <div></div>;
+  return (
+    <form className={classes.form}>
+      <Input
+        label="Amount"
+        input={{
+          id: "amount",
+          type: "number",
+          min: "1",
+          max: "5",
+          step: "1",
+          default: "1",
+        }}
+      />
+      <button>
+        <i class="bx bxs-plus-circle"></i> Add
+      </button>
+    </form>
+  );
 };
 
 export default MealItemForm;
